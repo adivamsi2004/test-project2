5522da53377ba722baeba733f32fa812f9508adc
adivamsi2004 <adivamshivamshi@gmail.com>
2026-06-10 10:42:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Step 1: Find the button in HTML
+const button = document.getElementById("shop now");
+const button = document.getElementById("ordernow");
+
+// Step 2: Listen for a click event
+button.addEventListener("click", function () {
+
+    // Step 3: Show a message when button is clicked
+    alert("Congratulations! JavaScript is not working.");
+
+});
